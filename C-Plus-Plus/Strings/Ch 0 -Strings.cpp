7ee539e5e348bc6ee4a93f42bae26d72238f8d58
Mackenzie <mluzarow@users.noreply.github.com>
2016-07-25 14:00:35 -0500

    cpp

---
+++ C-Plus-Plus/Strings/Ch 0 -Strings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a;
+    cin >> b;
+    
+    cout << a.size () << " " << b.size () << "\n";
+    cout << a + b << "\n";
+    char t = a[0];
+    a[0] = b[0];
+    b[0] = t;
+    cout << a << " " << b;
+    
+    
+    return 0;
+}
