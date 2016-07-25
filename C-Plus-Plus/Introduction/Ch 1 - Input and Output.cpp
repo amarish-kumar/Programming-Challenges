ca50dc77af4fe946004e473856a280558de098c6
Mackenzie <mluzarow@users.noreply.github.com>
2016-07-25 13:55:39 -0500

    cpp

---
+++ C-Plus-Plus/Introduction/Ch 1 - Input and Output.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c = 0;
+    
+    scanf ("%d %d %d", &a, &b, &c);
+    
+    cout << (a + b + c);
+    
+    return 0;
+}
